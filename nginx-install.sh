47ce4aeaa0070b3922f237d8519bcad0f4c08376
Michael Keyser <Erutan409@users.noreply.github.com>
2017-03-12 14:17:13 -0400

    nginx-install.sh - Removed `ls` and `rm` commands. - Updated line endings.

--- nginx-install.sh
+++ nginx-install.sh
@@ -1,5 +1,2 @@
 wget http://nginx.org/download/nginx-1.9.2.tar.gz
 git clone git://github.com/Erutan409/ngx_http_proxy_connect_module.git
-ls -l
-rm -rf nginx-1.9.2.tar.gz
-rm -rf ngx_http_proxy_connect_module
