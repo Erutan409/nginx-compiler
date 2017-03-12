e5fd3f75b8a425de73e5fe798b0180d06266add7
Michael Keyser <Erutan409@users.noreply.github.com>
2017-03-12 14:03:07 -0400

    nginx-install.sh - Adding file (current changes)

---
+++ nginx-install.sh
@@ -0,0 +1,5 @@
+wget http://nginx.org/download/nginx-1.9.2.tar.gz
+git clone https://github.com/Erutan409/ngx_http_proxy_connect_module.git
+ls -l
+rm -rf nginx-1.9.2.tar.gz
+rm -rf ngx_http_proxy_connect_module
